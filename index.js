8470142ebb59f31636ed46ee2023b87199421843
unknown <okpara_tochukwu@yahoo.com>
2021-10-14 07:34:28 +0100

    add: active class to nav a tag, logo create and included about section width resizing

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const mobileToggle = document.querySelector('#mobile_menu')
 const nav = document.querySelector('.nav-flex')
 const bodyfixed = document.querySelector('body')
 
+// const speakerDetails = [{
+//     speakerImg : './public/squaretile.png',
+//     speakerName: 'Aliko Dangote',
+//     speakerProfile: 'founder and ceo dangote group',
+//     profileDetails: 'Lorem ipsum dolnesciunt eius ipsum incidunt nemo commodi laborum porro temporibus laudantium placeat. An'
+// }
+// ]
+
 // Display mobile menu
 
 const mobilemenu = () => {
